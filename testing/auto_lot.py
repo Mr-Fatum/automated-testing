1ae4b573a1bc1f6900ade4e2b1941449626d68e8
Iluchkinn <114167306+Mr-Fatum@users.noreply.github.com>
2025-08-19 17:31:17 +0500

    finally got it

--- testing/auto_lot.py
+++ testing/auto_lot.py
@@ -5,6 +5,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import time
 
+def body_click():
+    # После выбора опции кликните на ней или вне dropdown
+    option.click()
+    time.sleep(0.5)
+
+    # Клик в любое другое место страницы чтобы свернуть
+    body = driver.find_element(By.TAG_NAME, "body")
+    body.click()
+
 driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 URL = "https://ompk.gpms.naumen.ru/fx/"
@@ -28,7 +37,7 @@ try:
     button = driver.find_element(By.XPATH, "//*[@id=\"gwtAddSrmPurchase\"]")
     button.click()
 
-    # Ждем появления dropdown
+    # Ждем появления dropdown метода закупки
     dropdown = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//select[.//option[contains(text(), 'Закупка')]]"))
     )
@@ -41,6 +50,35 @@ try:
     option = driver.find_element(By.XPATH,
                                  "//option[contains(text(), 'Закупка (вспомогательные материалы, технические закупки)')]")
     option.click()
+    body_click()
+
+    signature_dropdown = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//td[contains(text(), 'Подпись предложения обязательна')]/following-sibling::td//select"))
+)
+
+    signature_dropdown.click()
+    time.sleep(1)
+    option_sign = driver.find_element(By.XPATH, "//option[contains(text(), 'нет')]")
+    option_sign.click()
+
+    time.sleep(1)
+    body_click()
+
+    name_textarea = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, "//td[contains(text(), 'Наименование закупки')]/following-sibling::td//textarea"))
+    )
+
+    name_textarea.clear()
+    name_textarea.send_keys("automatically created")
+    time.sleep(1)
+
+    add_button_submit = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH,
+                                    "//button[.//div[contains(text(), 'Добавить')] and not(ancestor::div[contains(@style,'display: none')])]"))
+    )
+    add_button_submit.click()
+
 
     print("Успешно выбрано!")
 
@@ -53,3 +91,4 @@ except Exception as e:
 finally:
     time.sleep(10)
     driver.quit()
+
